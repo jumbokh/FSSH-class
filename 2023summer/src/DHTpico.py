07133d8960a6ad53f6fb08226867d18f83c0c904
jumbokh <jumbokh@gmail.com>
2023-07-12 10:47:41 +0800

    add dht11

---
+++ 2023summer/src/DHTpico.py
@@ -0,0 +1,12 @@
+from machine import Pin, I2C
+import utime as time
+from dht import DHT11, InvalidChecksum
+
+while True:
+    time.sleep(5)
+    pin = Pin(28, Pin.OUT, Pin.PULL_DOWN)
+    sensor = DHT11(pin)
+    t  = (sensor.temperature)
+    h = (sensor.humidity)
+    print("Temperature: {}".format(sensor.temperature))
+    print("Humidity: {}".format(sensor.humidity))
